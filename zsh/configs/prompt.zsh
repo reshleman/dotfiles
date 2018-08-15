7b41f9ac753253fd5f2ecec7fc8ae4d5bb3292ce
Robert Eshleman <c.robert.eshleman@gmail.com>
2018-08-15 11:46:15 -0400

    [zsh] Clean up prompt
    - Remove unnecessary variables
    - Add helper functions for color formatting
    - Update
    
    Color helpers h/t:
    https://github.com/gabebw/dotfiles/blob/08e247ce7ec8e72500a917a05c2e22c80b413311/zshrc#L432-L440

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,36 +1,40 @@
 setopt promptsubst
 
-# Variables
-ZSH_THEME_DIR_PROMPT_PREFIX="%{$fg[green]%}"
-ZSH_THEME_DIR_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_DIR_PROMPT="%c"
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[yellow]%}("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="*"
-ZSH_THEME_GIT_PROMPT_CLEAN=""
-ZSH_THEME_CMD_PROMPT=" %(!.#.$) "
+# Color helpers
+prompt_color()  { echo "%{$fg[$2]%}$1%{$reset_color%}" }
+prompt_green()  { prompt_color "$1" green }
+prompt_magenta(){ prompt_color "$1" magenta }
+prompt_purple() { prompt_color "$1" purple }
+prompt_red()    { prompt_color "$1" red }
+prompt_cyan()   { prompt_color "$1" cyan }
+prompt_blue()   { prompt_color "$1" blue }
+prompt_yellow() { prompt_color "$1" yellow }
+prompt_spaced() { [[ -n "$1" ]] && print " $@" }
 
-# Is the working directory clean?
+# Show an asterisk if the working directory is not clean.
 parse_git_dirty() {
   if [[ -n $(git status --porcelain) ]]; then
-    echo -n $ZSH_THEME_GIT_PROMPT_DIRTY
-  else
-    echo -n $ZSH_THEME_GIT_PROMPT_CLEAN
+    echo -n "*"
   fi
 }
 
 # Show the branch name or short SHA if we're in a git repo
 git_prompt_info() {
-  ref=$(git symbolic-ref --short HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null)
+  local ref=$(git symbolic-ref --short HEAD 2> /dev/null || git rev-parse --short HEAD 2> /dev/null)
   if [[ -n $ref ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref}$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    prompt_yellow "($ref$(parse_git_dirty)) "
   fi
 }
 
-# Concat the directory portion of the prompt
+# Trailing portion of the current working directory, or ~.
 dir_prompt_info() {
-  echo "$ZSH_THEME_DIR_PROMPT_PREFIX$ZSH_THEME_DIR_PROMPT$ZSH_THEME_DIR_PROMPT_SUFFIX"
+  prompt_green "%1~ "
+}
+
+# If the shell is running with priviliges (e.g., root), then #, else $.
+prompt_info() {
+  echo " %(!.#.$) "
 }
 
 # Concat the entire prompt
-export PS1='$(dir_prompt_info)$(git_prompt_info)$ZSH_THEME_CMD_PROMPT'
+export PS1='$(dir_prompt_info)$(git_prompt_info)$(prompt_info)'
