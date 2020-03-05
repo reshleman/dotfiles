a5a3f99c403f2ec95274c9c0bb53b4edaac34b4e
Robert Eshleman <c.robert.eshleman@gmail.com>
2020-03-05 09:52:28 -0500

    Account for `asdf` being installed by Homebrew
    See: thoughtbot/dotfiles@2af1b501

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,9 +1,15 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# load ASDF
-if [ -d "$HOME/.asdf" ]; then
-  . $HOME/.asdf/asdf.sh
+# Load ASDF
+if [ -f "$HOME/.asdf/asdf.sh" ]; then
+  # Try loading ASDF from the regular home dir location
+  . "$HOME/.asdf/asdf.sh"
+elif which brew >/dev/null &&
+  # If ASDF has been installed via Homebrew, the completions file is in a different place
+  BREW_DIR="$(dirname $(which brew))/.." &&
+  [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
+  . "$BREW_DIR/opt/asdf/asdf.sh"
 fi
 
 # mkdir .git/safe in the root of repositories you trust
