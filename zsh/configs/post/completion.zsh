0421b00ad5b8880c7349c2cc6bf7244cb0a53b0f
Robert Eshleman <c.robert.eshleman@gmail.com>
2025-12-04 15:38:32 -0500

    [zsh] Speed up git completion

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -10,3 +10,9 @@ autoload -U bashcompinit && bashcompinit
 # Sourced because we are overriding an existing completion function, not
 # defining a new completion function:
 source ~/.zsh/completion/_git_checkout.zsh
+
+# Speed up painfully slow git completion
+# https://stackoverflow.com/a/9810485
+__git_files () {
+    _wanted files expl 'local files' _files
+}
